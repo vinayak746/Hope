736dee6cc08e654ae418b3fbca2be0a69dd9257c
Vinayak Arora <vinayakarora7461@gmail.com>
2025-08-03 19:14:48 +0530

    feat: add logo images and update Navbar component to use logo

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import { Link } from 'react-router'
+import logo from '/logo.png'
+
 
 const Navbar = () => {
   return (
     <nav className='navbar'>
         <Link to="/" >
-        <p className='text-2xl font-bold text-gradient' >HOPE</p>
+        {/* <p className='text-2xl font-bold text-gradient' >HOPE</p> */}
+        <img src={logo} alt="Hope Logo" className='w-24 h-24' />
         </Link>
         <Link to="/upload" className='primary-button w-fit'>
           Upload Resume
