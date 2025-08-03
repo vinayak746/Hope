5783145ef44b03fa5c879e654b1464acdca5161b
Vinayak Arora <vinayakarora7461@gmail.com>
2025-08-03 18:35:35 +0530

    fix: correct spacing in page heading of Home component

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -15,7 +15,7 @@ export default function Home() {
   return <main className="bg-[url('images/bg-main.svg')] bg-cover ">
     <Navbar />
     <section className="main-section">
-      <div className="page-heading py-16">
+      <div className="page-heading py-16 ">
         <h1>Track Your Applications & Resume Ratings</h1>
         <h2>Review your submissions and check AI-powered feedback.</h2>
       </div>
