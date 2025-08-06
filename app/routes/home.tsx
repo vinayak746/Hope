8409b2ac4bced1cd5f0380e12e799a521851ef53
Vinayak Arora <vinayakarora7461@gmail.com>
2025-08-06 19:01:52 +0530

    feat: integrate Puter store for authentication and routing in Home and Auth components

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -2,6 +2,9 @@ import ResumeCard from "../components/ResumeCard";
 import { resumes } from "../../constants/index";
 import Navbar from "../components/Navbar";
 import type { Route } from "./+types/home";
+import { usePuterStore } from "../lib/puter";
+import { useNavigate } from "react-router";
+import { useEffect } from "react";
 
 
 export function meta({}: Route.MetaArgs) {
@@ -12,6 +15,13 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
+    const { auth} = usePuterStore();
+   
+    const navigate = useNavigate();
+
+    useEffect(()=>{
+        if(!auth.isAuthenticated)navigate('/auth?next=/');
+    },[auth.isAuthenticated]);
   return <main className="bg-[url('images/bg-main.svg')] bg-cover ">
     <Navbar />
     <section className="main-section">
